876420f333f091f4bf5d0afa8df5979e5da7b910
bypie5 <bypie966@gmail.com>
2018-06-01 01:00:01 -0700

    Wrote LEDMatrix class

--- uart2led/uart2led.ino
+++ uart2led/uart2led.ino
@@ -4,8 +4,8 @@ class LED {
     LED(int ID, int * pins) {
       id = ID; // Pin value of the anode
       currColor = -1; // Off
-      calcPins();
       pinsInUse = pins;
+      calcPins();
     }
 
   private:
@@ -29,7 +29,11 @@ class LED {
       }
       
       if (id + 2 > pinsInUse[3]) {
-        pins[2] = pinsInUse[1];
+        if (id + 2 > pinsInUse[3] + 1)
+          pins[2] = pinsInUse[1];
+        else 
+          pins[2] = pinsInUse[0];
+         
       } else {
         pins[2] = id + 2;
       }    
@@ -119,28 +123,83 @@ class LEDColumn {
     
 };
 
+class LEDMatrix {
+public:
+  LEDMatrix(LEDColumn** columns, int sz) {
+    this->columns = columns;
+    this->sz = sz;
+  }
+
+private:
+  LEDColumn** columns;
+  int sz;
+
+public:
+  void draw(int col, int led, int color) {
+    columns[col]->setLED(led, color);
+    columns[col]->updateLEDs();
+  }
+
+  // Simple display to see if matrix is working correctly
+  void testv1() {
+    int dly = 50;
+    // Color
+    for(int color = 0; color < 3; ++color) {
+      // Column
+      for (int col = 0; col < sz; ++col) {
+        // LED
+        for (int led = 0; led < 4; ++led) {
+          // Delay
+          for (int i = 0; i < dly; ++i) {
+            draw(col, led, color);
+            delay(1);
+          }
+        }
+      }
+    }
+  }
+
+  
+  void testv2() {
+    int dly = 50;
+    // Color
+    for(int color = 0; color < 3; ++color) {
+        // LED
+        for (int led = 0; led < 4; ++led) {
+          // Delay
+          for (int i = 0; i < dly; ++i) {
+            draw(0, led, color);
+            draw(1, led, color);
+            draw(2, led, color);
+            draw(3, led, color);
+            delay(1);
+          }
+        }
+    }
+  }
+};
+
 int incomingByte = 0;
-LEDColumn * ledcolumn_0;
-LEDColumn * ledcolumn_1;
-LEDColumn * ledcolumn_2;
-LEDColumn * ledcolumn_3;
+LEDMatrix * matrix;
 void setup() {
   int colmunPins_0[4] = {4,5,6,7};
   int colmunPins_1[4] = {8,9,10,11};
   int colmunPins_2[4] = {12,13,14,15};
   int colmunPins_3[4] = {16,17,18,19};
-  
-  ledcolumn_0 = new LEDColumn(colmunPins_0);
-  ledcolumn_1 = new LEDColumn(colmunPins_1);
-  ledcolumn_2 = new LEDColumn(colmunPins_2);
-  ledcolumn_3 = new LEDColumn(colmunPins_3);
+  LEDColumn * ledcolumn_0 = new LEDColumn(colmunPins_0);
+  LEDColumn * ledcolumn_1 = new LEDColumn(colmunPins_1);
+  LEDColumn * ledcolumn_2 = new LEDColumn(colmunPins_2);
+  LEDColumn * ledcolumn_3 = new LEDColumn(colmunPins_3);
+
+  LEDColumn* columns[4] = {ledcolumn_0, ledcolumn_1, ledcolumn_2, ledcolumn_3};
+  matrix = new LEDMatrix(columns, 4);
   
   Serial.begin(9600);
 }
 
 void loop() { 
   // send data only when you receive data:
-  /*if (Serial.available() > 0) {
+  if (Serial.available() > 0) {
       // xxxx   xx  xx
       // column led color
       incomingByte = Serial.read();
@@ -149,15 +208,9 @@ void loop() {
   //unsigned char colmun
   unsigned char currLed = (incomingByte >> 2) & 0x03;
   unsigned char color = incomingByte & 0x03;
-  ledcolumn->setLED(currLed, 2);
-  ledcolumn->updateLEDs();*/
-  ledcolumn_0->setLED(3, 2);
-  ledcolumn_1->setLED(3, 2);
-  ledcolumn_2->setLED(3, 2);
-  ledcolumn_3->setLED(3, 2);
 
-  ledcolumn_0->updateLEDs();
-  ledcolumn_1->updateLEDs();
-  ledcolumn_2->updateLEDs();
-  ledcolumn_3->updateLEDs();
+  matrix->draw(0, 0, 0);
+  matrix->draw(1, 1, 1);
+  matrix->draw(2, 2, 2);
+  matrix->draw(3, 3, 0);
 }
