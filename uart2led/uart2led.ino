625e899410ac6ecd2912ad11465a3b7d2ec38c72
bypie5 <bypie966@gmail.com>
2018-06-01 00:12:18 -0700

    Generalized pins for LEDMatrix class and LED class

--- uart2led/uart2led.ino
+++ uart2led/uart2led.ino
@@ -1,33 +1,35 @@
 // Red, Anode, Blue, Green
 class LED {
   public:
-    LED(int ID) {
+    LED(int ID, int * pins) {
       id = ID; // Pin value of the anode
       currColor = -1; // Off
       calcPins();
+      pinsInUse = pins;
     }
 
   private:
     int id;
     int pins[3];
     int currColor;
+    int * pinsInUse; // Just for context of the pin range, ie {4,5,6,7}
 
     // Loops around the pins to find color pins based on location of anode
     void calcPins() {
-      if (id - 1 < 4) {
-        pins[0] = 7;
+      if (id - 1 < pinsInUse[0]) {
+        pins[0] =  pinsInUse[3];
       } else {
         pins[0] = id - 1;
       }
-
-      if (id + 1 > 7) {
-        pins[1] = 4;
+      
+      if (id + 1 > pinsInUse[3]) {
+        pins[1] = pinsInUse[0];
       } else {
         pins[1] = id + 1;
       }
-
-      if (id + 2 > 7) {
-        pins[2] = 4;
+      
+      if (id + 2 > pinsInUse[3]) {
+        pins[2] = pinsInUse[1];
       } else {
         pins[2] = id + 2;
       }    
@@ -90,7 +92,7 @@ class LEDColumn {
     LEDColumn(int * pinsInUse) {
       pins = pinsInUse;
       for (int i = 0; i < 4; i++) {
-        leds[i] = new LED(pins[i]);
+        leds[i] = new LED(pins[i], pins);
         pinMode(pins[i], INPUT);
       }  
     }
@@ -118,17 +120,27 @@ class LEDColumn {
 };
 
 int incomingByte = 0;
-LEDColumn * ledcolumn;
+LEDColumn * ledcolumn_0;
+LEDColumn * ledcolumn_1;
+LEDColumn * ledcolumn_2;
+LEDColumn * ledcolumn_3;
 void setup() {
-  int colmunPins[4] = {4,5,6,7};
-  ledcolumn = new LEDColumn(colmunPins);
+  int colmunPins_0[4] = {4,5,6,7};
+  int colmunPins_1[4] = {8,9,10,11};
+  int colmunPins_2[4] = {12,13,14,15};
+  int colmunPins_3[4] = {16,17,18,19};
+  
+  ledcolumn_0 = new LEDColumn(colmunPins_0);
+  ledcolumn_1 = new LEDColumn(colmunPins_1);
+  ledcolumn_2 = new LEDColumn(colmunPins_2);
+  ledcolumn_3 = new LEDColumn(colmunPins_3);
   
   Serial.begin(9600);
 }
 
 void loop() { 
   // send data only when you receive data:
-  if (Serial.available() > 0) {
+  /*if (Serial.available() > 0) {
       // xxxx   xx  xx
       // column led color
       incomingByte = Serial.read();
@@ -138,5 +150,14 @@ void loop() {
   unsigned char currLed = (incomingByte >> 2) & 0x03;
   unsigned char color = incomingByte & 0x03;
   ledcolumn->setLED(currLed, 2);
-  ledcolumn->updateLEDs();
+  ledcolumn->updateLEDs();*/
+  ledcolumn_0->setLED(3, 2);
+  ledcolumn_1->setLED(3, 2);
+  ledcolumn_2->setLED(3, 2);
+  ledcolumn_3->setLED(3, 2);
+
+  ledcolumn_0->updateLEDs();
+  ledcolumn_1->updateLEDs();
+  ledcolumn_2->updateLEDs();
+  ledcolumn_3->updateLEDs();
 }
